6bc96b0a8e88ab906a399ab7a99a71baeed2cf5d
unknown <walid.el.biir@gmail.com>
2024-04-25 20:39:31 +0100

    jenkins-shared-library

---
+++ Jenkins-Shared-Library/vars/npm.groovy
@@ -0,0 +1,12 @@
+def install_dependencies() {
+    sh 'npm install'
+}
+
+def install_backend_test_dependencies() {
+    sh 'npm i --save-dev chai chai-http mocha sinon'
+    sh 'npm i --save-dev mongodb-memory-server --ignore-scripts'
+}
+
+def backend_test() {
+    sh 'NODE_ENV=test npm test ./tests'
+}
