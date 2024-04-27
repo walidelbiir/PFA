8784216fc634968a6dab4f830ab28a97d09dd707
unknown <walid.el.biir@gmail.com>
2024-04-27 16:11:23 +0100

    added a call method to git.groovy

--- Jenkins-Shared-Library/vars/git.groovy
+++ Jenkins-Shared-Library/vars/git.groovy
@@ -1,5 +1,7 @@
 def checkout(String credentialsId , String githubURL) {
     git branch: 'main', credentialsId: credentialsId , url: githubURL
 }
-
+def call(String credentialsId , String githubURL) {
+    checkout(credentialsId , githubURL)
+}
 return this
