e1653601a064c46b1d4b813eb955d53e39bfe6c8
unknown <walid.el.biir@gmail.com>
2024-04-29 19:19:08 +0100

    fixed syntax

--- Jenkins-Shared-Library/vars/slackNotification.groovy
+++ Jenkins-Shared-Library/vars/slackNotification.groovy
@@ -2,7 +2,7 @@ def send(Map params) {
     channel = params.channel
     msg = params.msg
     status = params.status
-    slackSend channel: channel , message: msg , color: status
+    slackSend channel: "${channel}" , message: "${msg}" , color: "${status}"
 }
 
 def uploadFile(String path , String channel = "#pfa,walid.elbir" , String comment) {
