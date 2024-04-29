4251032a5ebd733c7fdf63fac5eacb112e2771c7
unknown <walid.el.biir@gmail.com>
2024-04-29 16:32:34 +0100

    changed file name slack to slackNotification

--- Jenkins-Shared-Library/vars/slack.groovy
+++
@@ -1,9 +0,0 @@
-def sendSlackNotification(String channel = "#pfa,walid.elbir" , String msg , String status) {
-    slackSend channel: channel , message: msg , color: status
-}
-
-def uploadFileSlack(String path , String channel = "#pfa,walid.elbir" , String comment) {
-    slackUploadFile filePath: path, channel: channel, initialComment: comment
-}
-
-return this
