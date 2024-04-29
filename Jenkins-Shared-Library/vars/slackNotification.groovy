7ca7e35cd9cc27447e534b92af2340b890d4771d
unknown <walid.el.biir@gmail.com>
2024-04-29 16:43:54 +0100

    test

--- Jenkins-Shared-Library/vars/slackNotification.groovy
+++ Jenkins-Shared-Library/vars/slackNotification.groovy
@@ -1,4 +1,7 @@
-def send(String channel = "#pfa,walid.elbir" , String msg , String status) {
+def send(Map params) {
+    channel = params.channel
+    msg = params.msg
+    status = params.status
     slackSend channel: channel , message: msg , color: status
 }
 
